13f7e08c319de94911e61ff3b25d56ddf20e0140
Aleksandr Ignatyev <vigntom@gmail.com>
2024-02-01 05:16:51 +0300

    Задание 3

--- src/main.tf
+++ src/main.tf
@@ -39,3 +39,42 @@ resource "yandex_compute_instance" "platform" {
   }
 
 }
+
+resource "yandex_vpc_subnet" "develop_db" {
+  name           = var.vm_db_vpc_name
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.vm_db_cidr
+}
+
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_family
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+ }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_db.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_db_is_preemptable
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "${var.vm_db_root_login}:${var.vms_ssh_root_key}"
+  }
+
+}
